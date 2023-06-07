b2ae511d1d3203037e727eb1f85c7d5cb13465de
akshaysatyam2 <akshaysatyam2003@gmail.com>
2023-06-07 16:59:18 +0530

    Working on Bank Customer Project using Deep Learning(ANN). Changed settings for Django project also added urls, apps, etc.

--- Django/BankCustomer/BankCustomer/settings.py
+++ Django/BankCustomer/BankCustomer/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Checkcustomer.apps.CheckcustomerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
